4ce7638abfbc6931c871aee4af4d0b17ec7f6b10
Harshith <harshithgv.sangamone@gmail.com>
2023-01-11 11:47:17 +0530

    first commit

---
+++ src/routes/Login.svelte
@@ -0,0 +1,76 @@
+<div class="container text-center">
+    <div class="mt-5">
+    <h1>WORK FROM HOME JUST GOT EASIER</h1>
+ </div>
+ <form class="row" id="form" novalidate>
+    
+   
+    
+    <div class="col">
+        
+        <select class="form-select" id="validationCustom04" required>
+          <option selected disabled value="">Select City</option>
+          <option>Bangalore</option>
+          <option>Mangalore</option>
+          <option>Mysore</option>
+          <option>Hasan</option>
+          <option>Chikbalapur</option>
+          <option>Doddabalapur</option>
+      
+        </select>
+      
+    </div><br> <br>
+    <div class="col">
+       
+        <select class="form-select" id="validationCustom04">
+          <option selected disabled value="">Select Hospital</option>
+          <option>Fortis</option>
+          <option>Jayadeva</option>
+          <option>Apollo</option>
+          <option>Manipal</option>
+          <option>Shekar Nursing Home</option>
+          <option>Shanthi Nursing Home</option>
+      
+        </select>
+        
+      </div><br><br>
+    <div class="col">
+      
+      <input type="date"class="form-control" id="validationCustom05"  placeholder="Select Date">
+      
+    </div>
+    <br><br>
+    <div class="col">
+       
+        <select class="form-select" id="validationCustom04" required>
+            <option selected disabled value="">Select Time</option>
+            <option>09:00 AM - 09:30 AM IST</option>
+            <option>10:00 AM IST</option>
+            <option>11:00 AM IST</option>
+            <option>12:00 AM IST</option>
+            <option>01:00 PM IST</option>
+            <option>02:00 AM IST</option>
+        
+          </select>
+      </div>
+    
+    <div class="col mt-3">
+      <a href="/Payment"><button class="btn btn-primary" type="submit">Submit form</button></a>
+    </div>
+  </form>
+    
+</div>
+
+
+<style>
+    #form {
+    --bs-gutter-x: 1.5rem;
+    --bs-gutter-y: 0;
+    display: inline-block;
+    flex-wrap: wrap;
+    margin-top: calc(-1 * var(--bs-gutter-y));
+    margin-right: calc(-.5 * var(--bs-gutter-x));
+    margin-left: calc(-.5 * var(--bs-gutter-x));
+}
+
+</style>
---
+++ src/routes/Payment.svelte
@@ -0,0 +1,7 @@
+<div class="text-center">
+    <h2>Payment</h2>
+    <h4>Please Pay following charges to book your Navatar for minimum 30 mins.</h4>
+    <h2>Rs. 10</h2>
+    <br>
+   <a href="https://pages.razorpay.com/navatar1"><button type="submit" class="btn btn-primary" id="pay">Pay</button></a>
+</div>
